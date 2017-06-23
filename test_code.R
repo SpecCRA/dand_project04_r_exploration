cd188d571cfe2347416a912fa91624e452b2d3a4
Benjamin Xiao <tiny@BenjamisMacBook.lan>
2017-06-23 04:19:25 -0700

    added data set and test code script

---
+++ test_code.R
@@ -0,0 +1,14 @@
+# Things to run first
+install.packages("ggplot2", dependencies = T) 
+install.packages("knitr", dependencies = T)
+install.packages("dplyr", dependencies = T)
+setwd("~/dand_files/project04_files/")
+
+# Libraries to load
+library(ggplot2)
+library(dplyr)
+library(knitr)
+library(gridExtra)
+
+#csv file to load
+pd <- read.csv("2016_17_nba_player_data.csv")
